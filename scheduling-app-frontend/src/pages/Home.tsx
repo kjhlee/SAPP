408053b27974d3df0a95402e8e89fc3a1d681841
KJ Lee <kunnyjlee@gmail.com>
2025-03-11 13:00:44 -0700

    created the ability to create a schedule

--- scheduling-app-frontend/src/pages/Home.tsx
+++ scheduling-app-frontend/src/pages/Home.tsx
@@ -1,7 +1,44 @@
+import {useState, useEffect} from "react";
+import {Schedule, ScheduleItem } from "../types/schedule";
+
+
 function Home(){
+    const [schedules, setSchedules] = useState<Schedule[]> ([]);
+    const fetchSchedules = () => {
+        fetch("http://localhost:8080/schedules/all")
+            .then((res) => res.json())
+            .then((data) => setSchedules(data))
+            .catch((err) => console.error("Error fetching schedules: ", err));
+    };
+
+    useEffect(() => {
+        fetchSchedules();
+    }, [])
+
+    const handleCreateSchedule = () =>{
+        // newSchedule payload since it should always be empty
+        const newSchedule = {
+            scheduleItems: []
+        };
+
+        try{
+            fetch(`http://localhost:8080/schedules/add`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newSchedule)
+            });
+            fetchSchedules();
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return(
         <div>
-            <h1>Hello this is the home page</h1>
+            <h1>SAPP</h1>
+            <p>by: kj Lee</p>
+            <button onClick = {handleCreateSchedule}>+ Add Schedule</button>
         </div>
     );
 }
