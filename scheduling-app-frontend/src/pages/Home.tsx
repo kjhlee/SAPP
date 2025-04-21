f622e5f37c801d4b5a9fc5f6e1ba5205085335b1
KJ Lee <kunnyjlee@gmail.com>
2025-04-21 16:04:40 -0700

    adding links instead of a tags

--- scheduling-app-frontend/src/pages/Home.tsx
+++ scheduling-app-frontend/src/pages/Home.tsx
@@ -1,14 +1,16 @@
 import "./styles/Home.css"
 
+import { Link } from "react-router-dom";
+
 function Home() {
     return(
         <div className = "main">
             <h1>SAPP</h1>
             <p>by: kj Lee</p>
             <p>This is the home page</p>
-            <a href="https://sapp-frontend.onrender.com/login">Login</a>
-            <br></br>
-            <a href="https://sapp-frontend.onrender.com/ScheduleList">Schedules</a>
+            <Link to="/login">Login</Link>
+            <br />
+            <Link to="/ScheduleList">Schedules</Link>
         </div>
     )
 }
