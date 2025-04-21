36a4e8eacc1427bdacd426544dd4772ba0566dae
KJ Lee <kunnyjlee@gmail.com>
2025-04-21 16:15:24 -0700

    fixed the links for cors

--- Scheduling-app-backend/src/main/java/com/schedulingApp/Scheduling/app/config/SecurityConfig.java
+++ Scheduling-app-backend/src/main/java/com/schedulingApp/Scheduling/app/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://sapp-frontend.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
